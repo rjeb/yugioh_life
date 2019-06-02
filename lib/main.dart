1acab8dc0331f4ab92f95f715c98db6b2bfc99cc
Jason <jasonnthaii@gmail.com>
2019-06-02 13:52:49 -0700

    changed size of elements for different screen sizes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:numberpicker/numberpicker.dart';
+import 'dart:math';
 
 
 void main() => runApp(MyApp());
@@ -42,6 +43,11 @@ class _MyHomePageState extends State<MyHomePage> {
   int _p2Hundredth = 0;
   int _p2Tenth = 0;
   int _p2Ones = 0;
+
+  IconData _coin = Icons.copyright;
+  IconData _dice = Icons.casino;
+
+  final _random = new Random();
 
   final List<MapEntry<String, String>> _trackedLP = List<MapEntry<String, String>>();
 
@@ -152,6 +158,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenHeight = MediaQuery.of(context).size.height;
+
     setPortraitOrientation();
     final _style = Theme.of(context).textTheme.display1;
     // This method is rerun every time setState is called, for instance as done
@@ -181,11 +191,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: <Widget>[
                     Text(
                       'Player Two LP',
-                      style: TextStyle(fontSize: 35),
+                      style: TextStyle(fontSize: _screenHeight/10),
                     ),
                     Text(
                       '$_p2Counter',
-                      style: TextStyle(fontSize: 70, color: Colors.grey),
+                      style: TextStyle(fontSize: _screenHeight/5, color: Colors.grey),
                     ),
                     Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -196,32 +206,32 @@ class _MyHomePageState extends State<MyHomePage> {
                               minValue: 0,
                               maxValue: 9,
                               onChanged: (value) => setState(() => _p2Thousandth = value),
-                          listViewWidth: 30,
-                          itemExtent: 30,),
+                          listViewWidth: 45,
+                          itemExtent: _screenHeight/14,),
                           NumberPicker.integer(
                             infiniteLoop: true,
                             initialValue: _p2Hundredth,
                             minValue: 0,
                             maxValue: 9,
                             onChanged: (value) => setState(() => _p2Hundredth = value),
-                            listViewWidth: 30,
-                          itemExtent: 30,),
+                            listViewWidth: 45,
+                          itemExtent: _screenHeight/14,),
                           NumberPicker.integer(
                             infiniteLoop: true,
                             initialValue: _p2Tenth,
                             minValue: 0,
                             maxValue: 9,
                             onChanged: (value) => setState(() => _p2Tenth = value),
-                            listViewWidth: 30,
-                            itemExtent: 30,),
+                            listViewWidth: 45,
+                            itemExtent: _screenHeight/14,),
                           NumberPicker.integer(
                             infiniteLoop: true,
                             initialValue: _p2Ones,
                             minValue: 0,
                             maxValue: 9,
                             onChanged: (value) => setState(() => _p2Ones = value),
-                            listViewWidth: 30,
-                            itemExtent: 30,),
+                            listViewWidth: 45,
+                            itemExtent: _screenHeight/14,),
                         ]
                     ),
                     Text('   '),
@@ -249,11 +259,11 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Text(
                   'Player One LP',
-                  style: TextStyle(fontSize: 35),
+                  style: TextStyle(fontSize: _screenHeight/10),
                 ),
                 Text(
                   '$_p1Counter',
-                  style: TextStyle(fontSize: 70, color: Colors.grey),
+                  style: TextStyle(fontSize: _screenHeight/5, color: Colors.grey),
                 ),
                 Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -264,32 +274,32 @@ class _MyHomePageState extends State<MyHomePage> {
                         minValue: 0,
                         maxValue: 9,
                         onChanged: (value) => setState(() => _p1Thousandth = value),
-                        listViewWidth: 30,
-                        itemExtent: 30,),
+                        listViewWidth: 45,
+                        itemExtent: _screenHeight/14,),
                       NumberPicker.integer(
                         infiniteLoop: true,
                         initialValue: _p1Hundredth,
                         minValue: 0,
                         maxValue: 9,
                         onChanged: (value) => setState(() => _p1Hundredth = value),
-                        listViewWidth: 30,
-                        itemExtent: 30,),
+                        listViewWidth: 45,
+                        itemExtent: _screenHeight/14,),
                       NumberPicker.integer(
                         infiniteLoop: true,
                         initialValue: _p1Tenth,
                         minValue: 0,
                         maxValue: 9,
                         onChanged: (value) => setState(() => _p1Tenth = value),
-                        listViewWidth: 30,
-                        itemExtent: 30,),
+                        listViewWidth: 45,
+                        itemExtent: _screenHeight/14,),
                       NumberPicker.integer(
                         infiniteLoop: true,
                         initialValue: _p1Ones,
                         minValue: 0,
                         maxValue: 9,
                         onChanged: (value) => setState(() => _p1Ones = value),
-                        listViewWidth: 30,
-                        itemExtent: 30,),
+                        listViewWidth: 45,
+                        itemExtent: _screenHeight/14,),
                     ]
                 ),
                 Text('   '),
@@ -316,56 +326,99 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       persistentFooterButtons: <Widget>[
-        Text('Coin & Dice'),
-        IconButton(
-          icon: Icon(Icons.casino),
-          onPressed: (){
-            Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => RandomEvents()),);
-          },
-        ),
-        Text('                                              Reset'),
-        IconButton(
-          icon: Icon(Icons.autorenew),
-          onPressed: (){
-            setState(() {
-              _p1Counter = 8000;
-              _p2Counter = 8000;
-              _trackedLP.clear();
-            });
-          },
-        ),
-        Text('                                        Duel Logs'),
-        IconButton(
-          icon: Icon(Icons.book),
-          onPressed: (){
-            Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                    builder: (BuildContext context) {
-                      final Iterable<ListTile> tiles = _trackedLP.map(
-                            (MapEntry<String, String> entry) {
-                          return ListTile(
-                            leading: Text(entry.key),
-                            trailing: Text(entry.value),
-                          );
-                        },
-                      );
-                      final List<Widget> listed = ListTile.divideTiles(
-                        context: context,
-                        tiles: tiles,
-                      ).toList();
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Coin & Dice'),
+            IconButton(
+              icon: Icon(Icons.casino),
+              onPressed: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                        builder: (BuildContext context) {
+                          return Scaffold(
+                            appBar: AppBar(
+                              title: Text('Coin and Dice'),
+                            ),
+                            body: Center(
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                        Text('Coin'),
+                                        IconButton(
+                                          icon: Icon(_coin, size: 100,),
+                                          onPressed: (){
+                                            if (_random.nextInt(2) == 1){
+                                              setState(() {
+                                                _coin = Icons.autorenew;
+                                              });
+                                            }
+                                            else{
+                                              setState(() {
+                                                _coin = Icons.brightness_1;
+                                              });
+                                            }
+                                          },
+                                        ),
+                                      ],
+                                    ),
 
-                      return Scaffold(
-                        appBar: AppBar(
-                          title: Text('Lifepoint Calculations'),
-                        ),
-                        body: ListView(children: listed),
-                      );
-                    }
-                )
-            );
-          },
+
+                                  ]
+                              ),
+                            ),
+                          );
+                        }
+                    )
+                );
+              },
+            ),
+            Text('Reset'),
+            IconButton(
+              icon: Icon(Icons.autorenew),
+              onPressed: (){
+                setState(() {
+                  _p1Counter = 8000;
+                  _p2Counter = 8000;
+                  _trackedLP.clear();
+                });
+              },
+            ),
+            Text('Duel Logs'),
+            IconButton(
+              icon: Icon(Icons.book),
+              onPressed: (){
+                Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                        builder: (BuildContext context) {
+                          final Iterable<ListTile> tiles = _trackedLP.map(
+                                (MapEntry<String, String> entry) {
+                              return ListTile(
+                                leading: Text(entry.key),
+                                trailing: Text(entry.value),
+                              );
+                            },
+                          );
+                          final List<Widget> listed = ListTile.divideTiles(
+                            context: context,
+                            tiles: tiles,
+                          ).toList();
+
+                          return Scaffold(
+                            appBar: AppBar(
+                              title: Text('Lifepoint Calculations'),
+                            ),
+                            body: ListView(children: listed),
+                          );
+                        }
+                    )
+                );
+              },
+            ),
+          ],
         ),
       ],
     );
@@ -380,11 +433,9 @@ class RandomEvents extends StatelessWidget {
         title: Text("Coin and Dice"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {//go back to LP menu
-            Navigator.pop(context);
-          },
-          child: Text('Return'),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
         ),
       ),
     );
